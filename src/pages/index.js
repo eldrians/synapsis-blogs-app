f631a5ce9dc40db91f544f2a69e1822a8efcee74
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-10 17:25:28 +0700

    test page

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            Get started by editing&nbsp; AXel Eldrian Hadiwibowo
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
