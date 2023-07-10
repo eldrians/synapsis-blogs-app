7a90bcd1748085704f53e274765a0b2894ddb0a8
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-11 02:25:43 +0700

    app(v1): added profile pages

---
+++ src/pages/profile/[id].js
@@ -0,0 +1,61 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const profile = ({ data }) => {
+  return (
+    <>
+      <Layout>
+        <div>
+          <p>{data.id}</p>
+          <p>{data.name}</p>
+          <p>{data.email}</p>
+          <p>{data.gender}</p>
+          <p>{data.status}</p>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default profile;
+
+export const getStaticPaths = async () => {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+  };
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/users`, {
+    method: "GET",
+    headers: headers,
+  });
+  const data = await res.json();
+
+  const paths = data.map((item) => {
+    return {
+      params: { id: item.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+  };
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/users/${id}`, {
+    method: "GET",
+    headers: headers,
+  });
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
