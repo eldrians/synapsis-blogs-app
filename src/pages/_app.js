409c90a765d9ca520a79d08a8dddaba7584281a3
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-12 04:16:09 +0700

    app(v2): fix error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,15 @@
 import NavBar from "@/components/NavBar";
 import "@/styles/globals.css";
-import { Montserrat } from "next/font/google";
+// import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import Footer from "@/components/Footer";
 import { useRouter } from "next/router";
 import { AnimatePresence } from "framer-motion";
 
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-mont",
-});
+// const montserrat = Montserrat({
+//   subsets: ["latin"],
+//   variable: "--font-mont",
+// });
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
@@ -18,8 +18,11 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main
+      {/* <main
         className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}
+      > */}
+      <main
+        className={` bg-light dark:bg-dark w-full min-h-screen`}
       >
         <NavBar />
         <AnimatePresence mode="wait">
